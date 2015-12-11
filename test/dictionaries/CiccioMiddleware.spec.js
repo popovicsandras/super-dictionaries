2d035a2ef14be0c3bbf0d31cc70e16032cc3d93e
Andras Popovics <andras.popovics@workshare.com>
2015-12-11 16:24:34 +0000

    CiccioMiddleware.

---
+++ test/dictionaries/CiccioMiddleware.spec.js
@@ -0,0 +1,20 @@
+/* global beforeEach, describe, it, expect, sinon */
+
+'use strict';
+
+function CiccioMiddleware(request, response, next) {
+
+}
+
+describe('CiccioMiddleware', function() {
+
+    it('should authorize in case of accounts scope', function() {
+    
+        // Arrange
+        
+        // Act
+        CiccioMiddleware();
+        
+        // Assert
+    });
+});
