d886c232784b7cfe820163d031aa273fa7df1bd0
Andras Popovics <andras.popovics@workshare.com>
2016-02-05 18:08:18 +0000

    Storage first tests.

--- dictionaries/Storage.js
+++ dictionaries/Storage.js
@@ -2,6 +2,10 @@
 
 class DictionariesStorage {
 
+    constructor(config, options) {
+        options.monk(config.database.url).get('dictionaries');
+    }
+
 }
 
 module.exports = DictionariesStorage;
